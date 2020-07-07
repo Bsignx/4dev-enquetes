e7a4f3b2d92d878de184f9eb6a56d4a973740c65
Bruno Mariano <bsignx@gmail.com>
2020-07-07 10:34:46 -0300

    test: ensure RemoteAuthentication throwns UnexpectedError on default cases

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,4 +1,5 @@
-import { InvalidCredentialsError } from './../../../domain/errors/invalid-credentials-errors';
+import { UnexpectedError } from './../../../domain/errors/unexpected-error';
+import { InvalidCredentialsError } from './../../../domain/errors/invalid-credentials-error';
 import { mockAthentication } from '@/domain/test/mock-athentication';
 import { HttpPostClientSpy } from '@/data/test/mock-http-client';
 import { RemoteAuthentication } from './remote-authentication';
@@ -43,5 +44,32 @@ describe('RemoteAuthentication', () => {
     };
     const promise = sut.auth(mockAthentication());
     await expect(promise).rejects.toThrow(new InvalidCredentialsError());
+  });
+
+  test('Should throw UnexpectedError if HttpPostClient returns 400', async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.badRequest,
+    };
+    const promise = sut.auth(mockAthentication());
+    await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
+
+  test('Should throw UnexpectedError if HttpPostClient returns 500', async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.serverError,
+    };
+    const promise = sut.auth(mockAthentication());
+    await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
+
+  test('Should throw UnexpectedError if HttpPostClient returns 404', async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.notFound,
+    };
+    const promise = sut.auth(mockAthentication());
+    await expect(promise).rejects.toThrow(new UnexpectedError());
   });
 });
